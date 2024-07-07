32c1561518130fb4dc4b1c0af836528bef0e01ab
SC <sc.smilecoordinate@gmail.com>
2024-07-07 13:01:23 +0900

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,22 @@
-//menu
-jQuery(document).ready(function($) {
-    var $nav   = $('#navArea');
-    var $btn   = $('.toggle_btn');
-    var $mask  = $('#mask');
-    var open   = 'open'; // class
-    // menu open close
-    $btn.on( 'click', function() {
-        if ( ! $nav.hasClass( open ) ) {
-        $nav.addClass( open );
-        } else {
-        $nav.removeClass( open );
-        }
+(function($) {
+    $(document).ready(function() {
+        var $nav = $('#navArea');
+        var $btn = $('.toggle_btn');
+        var $mask = $('#mask');
+        var open = 'open'; // クラス名
+
+        // メニューの開閉
+        $btn.on('click', function() {
+            if (!$nav.hasClass(open)) {
+                $nav.addClass(open);
+            } else {
+                $nav.removeClass(open);
+            }
+        });
+
+        // マスクをクリックして閉じる
+        $mask.on('click', function() {
+            $nav.removeClass(open);
+        });
     });
-    // mask close
-    $mask.on('click', function() {
-        $nav.removeClass( open );
-    });
-    });
+})(jQuery);
