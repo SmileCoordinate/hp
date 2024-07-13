f7d71b71f1ccaee93633d37370707d28fe4a6020
SmileCoordinate <sc.smilecoordinate@gmail.com>
2024-07-13 14:42:03 +0900

    head,foot js

--- js/header.js
+++ js/header.js
@@ -1,12 +1,12 @@
 $(function() {
+    // ヘッダー読み込み
     $("#header-placeholder").load("header.html", function() {
-        // スクリプトをここに移動
+        // ヘッダー内スクリプト
         var $nav = $('#navArea');
         var $btn = $('.toggle_btn');
         var $mask = $('#mask');
         var open = 'open'; // クラス名
 
-        // メニューの開閉
         $btn.on('click', function() {
             if (!$nav.hasClass(open)) {
                 $nav.addClass(open);
@@ -15,15 +15,19 @@ $(function() {
             }
         });
 
-        // マスクをクリックして閉じる
         $mask.on('click', function() {
             $nav.removeClass(open);
         });
 
-        // ChatBot のスクリプト
         window.toggleChatWindow = function() {
             var chatWindow = document.getElementById("chat-window");
             chatWindow.style.display = (chatWindow.style.display === "none" || chatWindow.style.display === "") ? "block" : "none";
         };
+    });
+
+    // フッター読み込み
+    $("#footer-placeholder").load("footer.html", function() {
+        // フッター内スクリプト
+        document.getElementById('year').textContent = new Date().getFullYear();
     });
 });
