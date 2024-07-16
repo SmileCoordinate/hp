db0e77aaefe08e22eda436cee0b9f6222dafe29c
SmileCoordinate <sc.smilecoordinate@gmail.com>
2024-07-16 15:29:13 +0900

    accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,18 +1,21 @@
 var $j = jQuery.noConflict();
 
-$j(document).ready(function () {
-    $j(".accordion-wrap").on("click", function () {
-        var accordionText = $j(this).children().eq(1);
-        accordionText.slideToggle(500); // アニメーション速度を500ミリ秒に統一
-        $j(this).children().eq(0).toggleClass("accordion-no-bar");
-        $j(this).siblings().find(".accordion-header").removeClass("accordion-gold").removeClass("active");
-        $j(this).siblings().find(".accordion-header i").removeClass("rotate-fa");
-        $j(this).find(".accordion-header").toggleClass("accordion-gold").toggleClass("active");
-        $j(this).find(".fa").toggleClass("rotate-fa");
-        $j(".accordion-wrap .accordion-text").not(accordionText).slideUp(500); // アニメーション速度を500ミリ秒に統一
-    });
+(function ($) {
+    $(document).ready(function () {
+        // Your code here
+        $(".accordion-wrap").on("click", function () {
+            var accordionText = $(this).children().eq(1);
+            accordionText.slideToggle(500);
+            $(this).children().eq(0).toggleClass("accordion-no-bar");
+            $(this).siblings().find(".accordion-header").removeClass("accordion-gold").removeClass("active");
+            $(this).siblings().find(".accordion-header i").removeClass("rotate-fa");
+            $(this).find(".accordion-header").toggleClass("accordion-gold").toggleClass("active");
+            $(this).find(".fa").toggleClass("rotate-fa");
+            $(".accordion-wrap .accordion-text").not(accordionText).slideUp(500);
+        });
 
-    $j(".clickable").on("click", function () {
-        $j(this).toggleClass("rotated");
+        $(".clickable").on("click", function () {
+            $(this).toggleClass("rotated");
+        });
     });
-});
+})(jQuery);
