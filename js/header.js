9c6a1e3e5a886b717d817c6120c9417171728ffe
SmileCoordinate <sc.smilecoordinate@gmail.com>
2024-07-16 15:37:32 +0900

    head js

--- js/header.js
+++ js/header.js
@@ -1,44 +1,38 @@
-$(function() {
+var $j = jQuery.noConflict();
+
+$j(function() {
     // ヘッダー読み込み
-    $("#header-placeholder").load("header.html", function(response, status, xhr) {
-        if (status == "error") {
-            console.error("Error loading header: " + xhr.status + " " + xhr.statusText);
-        } else {
-            // ヘッダー内スクリプト
-            var $nav = $('#navArea');
-            var $btn = $('.toggle_btn');
-            var $mask = $('#mask');
-            var open = 'open'; // クラス名
+    $j("#header-placeholder").load("header.html", function() {
+        // ヘッダー内スクリプト
+        var $nav = $j('#navArea');
+        var $btn = $j('.toggle_btn');
+        var $mask = $j('#mask');
+        var open = 'open'; // クラス名
 
-            $btn.on('click', function() {
-                if (!$nav.hasClass(open)) {
-                    $nav.addClass(open);
-                } else {
-                    $nav.removeClass(open);
-                }
-            });
-
-            $mask.on('click', function() {
+        $btn.on('click', function() {
+            if (!$nav.hasClass(open)) {
+                $nav.addClass(open);
+            } else {
                 $nav.removeClass(open);
-            });
+            }
+        });
 
-            window.toggleChatWindow = function() {
-                var chatWindow = document.getElementById("chat-window");
-                chatWindow.style.display = (chatWindow.style.display === "none" || chatWindow.style.display === "") ? "block" : "none";
-            };
-        }
+        $mask.on('click', function() {
+            $nav.removeClass(open);
+        });
+
+        window.toggleChatWindow = function() {
+            var chatWindow = document.getElementById("chat-window");
+            chatWindow.style.display = (chatWindow.style.display === "none" || chatWindow.style.display === "") ? "block" : "none";
+        };
     });
 
     // フッター読み込み
-    $("#footer-placeholder").load("footer.html", function(response, status, xhr) {
-        if (status == "error") {
-            console.error("Error loading footer: " + xhr.status + " " + xhr.statusText);
-        } else {
-            // フッター内スクリプト
-            var yearSpan = document.getElementById('year');
-            if (yearSpan) {
-                yearSpan.textContent = new Date().getFullYear();
-            }
+    $j("#footer-placeholder").load("footer.html", function() {
+        // フッター内スクリプト
+        var yearSpan = document.getElementById('year');
+        if (yearSpan) {
+            yearSpan.textContent = new Date().getFullYear();
         }
     });
 });
