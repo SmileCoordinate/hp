9274bdb141ca0450f11f4270cdce74f970b17605
SmileCoordinate <sc.smilecoordinate@gmail.com>
2024-07-16 15:08:05 +0900

    tab

--- js/works-tab.js
+++ js/works-tab.js
@@ -1,7 +1,11 @@
 (function ($) {
     $(document).ready(function () {
+        console.log("Document ready");
+
         $(".tab").click(function () {
+            console.log("Tab clicked");
             var tabId = $(this).attr('id');
+            console.log("Tab ID: " + tabId);
 
             $(".tab").removeClass('selected unselected');
             $(this).addClass('selected');
